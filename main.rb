ad11b786fccc67d123e278f66f145d668619833c
Edin <edyfer2@live.com.au>
2024-04-29 23:15:50 +0200

    Add method definition

--- main.rb
+++ main.rb
@@ -2,4 +2,16 @@
 #include the hash output on cell. 
 
 class Board
+    def printBoard
+        row = 0
+        while row < 3
+            col = 0
+            while col < 3
+                print "#"
+                col += 1
+            end
+            puts "\n"
+            row += 1
+        end
+    end
 end
