2f12adfcf8100aac735fd0664c87ed8f48af7b12
Edin <edyfer2@live.com.au>
2024-05-15 20:20:24 +0200

    Add comment

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+#Add a module that will be used to group all the classes related to tic-tac-toe
+
 #Create a board with three rows and three columns. To show a clear view of the board for both human players,
 #include the hash output on cell. 
 class Board
