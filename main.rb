7f1d5c702e005917a2c0c774cdedf6942e950683
Edin <edyfer2@live.com.au>
2024-05-17 15:18:23 +0200

    Add and edit comments

--- main.rb
+++ main.rb
@@ -26,43 +26,44 @@ class Game
         #Display the grids of the board with | split character
         puts ' ' + @board[2][0] + ' | ' + @board[2][1] + ' | ' + @board[2][2]
     end
-    #Add a method where the tic-tac-toe is played
-     
+    #Add a method that enables the game of tic-tac-toe to be played
+        #Add a newline character by placing puts
 
-    #Add a method that checks if an individual grid nought or cross
-        #If a selected grid has blank line, return false
-        #If a selected grid has a nought or cross, return true
+    #Add a method that checks if an individual grid is a nought or cross
+        #If a selected grid has blank character, return true
+        #If a selected grid has a nought or cross, return false
     
     #Add a method where the opponent randomly chooses a grid
-        #Create a variable stores a random number between 0 and 8
-        #Keep selecting number between 0 and 8 until the grid does not have O or X via loop
+        #Create a variable stores a random coordinate between 0,0 and 2,2 as first integer and second integer is
+        #randomly chosen between 0 and 2 is convert to string with comma placed between
+        #Keep selecting grids between 0,0 and 2,2 until the grid does not have O or X via loop
         #Inside a loop, check if the particular grid has O or X using the instance method below
     
     #Add a method where the grid number selected either by the opponent or player is used to 
     #convert blank space to X or O
-        #If a number is 0 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 0,0 and is not X or O (using instance method, check_grid_exist),
         #change board[0][0] to O or X
-        #If a number is 1 and is not X or O (using instance method, check_grid_exist), 
+        #If a grid is 0,1 and is not X or O (using instance method, check_grid_exist), 
         #change board[0][1] to O or X
-        #If a number is 2 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 0,2 and is not X or O (using instance method, check_grid_exist),
         #change board[0][2] to O or X
-        #If a number is 3 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 1,0 and is not X or O (using instance method, check_grid_exist),
         #change board[1][0] to O or X
-        #If a number is 4 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 1,1 and is not X or O (using instance method, check_grid_exist),
         #change board[1][1] to O or X
-        #If a number is 5 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 1,2 and is not X or O (using instance method, check_grid_exist),
         #change board[1][2] to O or X
-        #If a number is 6 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 2,0 and is not X or O (using instance method, check_grid_exist),
         #change board[2][0] to O or X
-        #If a number is 7 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 2,1 and is not X or O (using instance method, check_grid_exist),
         #change board[2][1] to O or X
-        #If a number is 8 and is not X or O (using instance method, check_grid_exist),
+        #If a grid is 2,2 and is not X or O (using instance method, check_grid_exist),
         #change board[2][2] to O or X
 
     #Add a method where a player is instructed to select the grid
-        #Print 'Please select grid (0-8): '
-        #Use fgets to input a number between 0 and 9 and convert to integer
-        #Print "Player selected: grid_number" if a grid is replaced with O or X
+        #Print 'Please select grid, eg.(x,y): '
+        #Use fgets to input the grid between 0,0 and 2,2 and convert to integer
+        #Print "Player selected: coordinates" if a grid is replaced with O or X
     
 end
 #Create a class called Opponent where an opponent is created
@@ -101,4 +102,4 @@ end
 #Create a game object
 game = Game.new
 #Invoke the play method on the Game object so the players can verse each other on tic-tac-toe
-
+game.printBoard
